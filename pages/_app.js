bffe3aeb861de998e5b88f40006fc91f25bd5c29
allssu <zxcvb6615@gmail.com>
2021-10-12 08:48:17 +0900

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Amplify } from "aws-amplify";
+import awsConfigure from "../src/aws-exports"
+
+Amplify.configure(awsConfigure);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
